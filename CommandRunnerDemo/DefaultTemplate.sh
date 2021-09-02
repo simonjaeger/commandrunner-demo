015ec6d3b589122d12ce84fe6379b75da1b5c78e
simonjaeger <simon.jager@live.com>
2021-09-02 23:35:35 +0200

    Clean templates

--- CommandRunnerDemo/DefaultTemplate.sh
+++ CommandRunnerDemo/DefaultTemplate.sh
@@ -1,18 +1,18 @@
 ﻿#!/bin/bash
 
 # Variables. Keep short to save space.
-PTH="/tmp/[DOWNLOAD_FILE_NAME]";
-URI="[URI]";
+PTH="/tmp/[DOWNLOAD_FILE_NAME]"
+URI="[URI]"
 
 # Install required tools.
-apt-get -qq install wget >> /dev/null;
+apt-get -qq install wget >> /dev/null
 
 # Remove file if already present.
-rm -f $PTH;
+rm -f $PTH
 
 # Download file.
 wget -q $URI -O $PTH
 
 # Execute file.
-chmod +x $PTH;
-$PTH;
+chmod +x $PTH
+$PTH
